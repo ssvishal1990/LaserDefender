7de37ea0187977cac9576c23e7abc89eec5c5233
ssvishal1990 <ssvishal1990@gmail.com>
2022-04-28 13:19:11 +0530

    Introduced a very basic particle effect
    A very basic effect when player or enemy gets hit

--- Assets/script/HealthScript.cs
+++ Assets/script/HealthScript.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class HealthScript : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
     DamageDealer damageDealer;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -13,6 +14,7 @@ public class HealthScript : MonoBehaviour
         damageDealer = other.GetComponent<DamageDealer>();
         if(damageDealer != null){
             takeDamage(damageDealer.getDamage());
+            playHitEffect();
             damageDealer.Hit();
         }
     }
@@ -25,4 +27,12 @@ public class HealthScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    
+    void playHitEffect(){
+        if(hitEffect != null){
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+
 }
